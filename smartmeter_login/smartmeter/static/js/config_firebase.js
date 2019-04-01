b0a88a8d536613c3f64f678eb412358795ab9ec8
fernfic <a5820097@gmail.com>
2019-04-02 04:17:01 +0700

    fern2

--- smartmeter_login/smartmeter/static/js/config_firebase.js
+++
@@ -1,8 +0,0 @@
- var config_firebase = {
-    apiKey: "AIzaSyDHEPLFgaTRQyxWifIhl3qQPcALMIrgfqM",
-    authDomain: "smart-farm-27e2b.firebaseapp.com",
-    databaseURL: "https://smart-farm-27e2b.firebaseio.com",
-    projectId: "smart-farm-27e2b",
-    storageBucket: "smart-farm-27e2b.appspot.com",
-    messagingSenderId: "788362802638"
-  };
